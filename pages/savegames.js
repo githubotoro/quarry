3dd4ce6c468ea3c8ffd20a0f9940299f3baa7d62
githubotoro <yupuday.git@gmail.com>
2023-03-05 19:34:30 +0530

    more meta added :)

--- pages/savegames.js
+++ pages/savegames.js
@@ -23,6 +23,39 @@ const Savegames = () => {
 					content="width=device-width, initial-scale=1"
 				/>
 
+				<meta itemprop="name" content="The Quarry Savegames " />
+				<meta
+					itemprop="description"
+					content="Let's end the curse of the Hag of Hackett's Quarry together."
+				/>
+				<meta
+					itemprop="image"
+					content="https://raw.githubusercontent.com/githubotoro/quarry/main/public/assets/splash.png"
+				/>
+
+				<meta name="twitter:card" content="summary_large_image" />
+				<meta name="twitter:title" content="The Quarry Savegames" />
+				<meta
+					name="twitter:description"
+					content="Let's end the curse of the Hag of Hackett's Quarry together."
+				/>
+				<meta
+					name="twitter:image"
+					content="https://raw.githubusercontent.com/githubotoro/quarry/main/public/assets/splash.png"
+				/>
+
+				<meta property="og:url" content="https://quarry.vercel.app" />
+				<meta property="og:type" content="website" />
+				<meta property="og:title" content="The Quarry Savegames" />
+				<meta
+					property="og:description"
+					content="Let's end the curse of the Hag of Hackett's Quarry together."
+				/>
+				<meta
+					property="og:image"
+					content="https://raw.githubusercontent.com/githubotoro/quarry/main/public/assets/splash.png"
+				/>
+
 				<meta name="twitter:card" content="summary_large_image" />
 				<meta name="twitter:site" content="@yupuday" />
 				<meta name="twitter:account_id" content="1460479438911537152" />
