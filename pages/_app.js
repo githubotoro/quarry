ffb131994b57dcaeff6f3176ad674002d5adacc0
githubotoro <yupuday.git@gmail.com>
2023-03-06 08:35:14 +0530

    trying npm package

--- pages/_app.js
+++ pages/_app.js
@@ -5,23 +5,24 @@ import * as gtag from "../lib/gtag";
 import Script from "next/script";
 import { useRouter } from "next/router";
 import { useState, useEffect } from "react";
+import { GoogleAnalytics } from "nextjs-google-analytics";
 
 export default function App({ Component, pageProps }) {
-	const router = useRouter();
+	// const router = useRouter();
 
-	useEffect(() => {
-		const handleRouteChange = (url) => {
-			gtag.pageview(url);
-		};
-		router.events.on("routeChangeComplete", handleRouteChange);
-		return () => {
-			router.events.off("routeChangeComplete", handleRouteChange);
-		};
-	}, [router.events]);
+	// useEffect(() => {
+	// 	const handleRouteChange = (url) => {
+	// 		gtag.pageview(url);
+	// 	};
+	// 	router.events.on("routeChangeComplete", handleRouteChange);
+	// 	return () => {
+	// 		router.events.off("routeChangeComplete", handleRouteChange);
+	// 	};
+	// }, [router.events]);
 
 	return (
 		<>
-			<Script
+			{/* <Script
 				strategy="afterInteractive"
 				src={`https://www.googletagmanager.com/gtag/js?id=G-${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
 			/>
@@ -32,7 +33,9 @@ export default function App({ Component, pageProps }) {
 				dangerouslySetInnerHTML={{
 					__html: `window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}', { page_path: window.location.pathname, });`,
 				}}
-			/>
+			/> */}
+
+			<GoogleAnalytics trackPageViews />
 
 			<Navbar />
 			<Component {...pageProps} />
