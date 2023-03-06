66fcb24fde56241e19d904a0956d83988281ab36
githubotoro <yupuday.git@gmail.com>
2023-03-06 08:21:59 +0530

    added analytics again

--- pages/_app.js
+++ pages/_app.js
@@ -19,20 +19,19 @@ export default function App({ Component, pageProps }) {
 
 	return (
 		<>
-			<Head>
-				<Script
-					strategy="afterInteractive"
-					src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXX"
-				/>
+			<Script
+				strategy="afterInteractive"
+				src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXX"
+			/>
 
-				<Script
-					id="google-analytics"
-					strategy="afterInteractive"
-					dangerouslySetInnerHTML={{
-						__html: `window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}', { page_path: window.location.pathname, });`,
-					}}
-				/>
-			</Head>
+			<Script
+				id="google-analytics"
+				strategy="afterInteractive"
+				dangerouslySetInnerHTML={{
+					__html: `window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}', { page_path: window.location.pathname, });`,
+				}}
+			/>
+
 			<Navbar />
 			<Component {...pageProps} />
 		</>
