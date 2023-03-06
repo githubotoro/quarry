455ab9e318db33b6d2998654ff4697ff06e1352a
githubotoro <yupuday.git@gmail.com>
2023-03-06 08:20:47 +0530

    added analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,38 @@
 import "@/styles/globals.css";
 import { Navbar } from "@/components/Navbar";
+import Head from "next/head";
+import * as gtag from "../lib/gtag";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }) {
+	const router = useRouter();
+
+	useEffect(() => {
+		const handleRouteChange = (url) => {
+			gtag.pageview(url);
+		};
+		router.events.on("routeChangeComplete", handleRouteChange);
+		return () => {
+			router.events.off("routeChangeComplete", handleRouteChange);
+		};
+	}, [router.events]);
+
 	return (
 		<>
+			<Head>
+				<Script
+					strategy="afterInteractive"
+					src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXX"
+				/>
+
+				<Script
+					id="google-analytics"
+					strategy="afterInteractive"
+					dangerouslySetInnerHTML={{
+						__html: `window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}', { page_path: window.location.pathname, });`,
+					}}
+				/>
+			</Head>
 			<Navbar />
 			<Component {...pageProps} />
 		</>
